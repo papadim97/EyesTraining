3e054087debb9ef6eaeeff4ed9e8caa7917d2f8f
papadim97 <papadimdim@yahoo.gr>
2021-09-12 16:55:32 +0300

    real numbers with random seed

--- EyesTraining.ino
+++ EyesTraining.ino
@@ -1,10 +1,18 @@
+unsigned long myTime;
+const int buttonPin = 12;
+
 void setup() {
+  pinMode(buttonPin, INPUT);  
   pinModeLeds();
   testingLeds();
 }
 
 void loop() {
-  startTraining();
+  myTime = millis();
+  randomSeed(myTime);
+  if (digitalRead(buttonPin) == HIGH) {
+    startTraining();
+  }
 }
 
   void testingLeds() {
@@ -42,59 +50,61 @@ void loop() {
 }
 
   void startTraining() {
-    int Rand = random(2,10);   
-    if (Rand==2){
-       digitalWrite(2, HIGH ); // Turn the LED on
-       delay(2000);
-       digitalWrite(2, LOW ); // Turn the LED off
-       delay(2000);
-    }
-    if (Rand==3){
-       digitalWrite(3, HIGH ); // Turn the LED on
-       delay(2000);
-       digitalWrite(3, LOW ); // Turn the LED off
-       delay(2000);
-    }
-    if (Rand==4){
-       digitalWrite(4, HIGH ); // Turn the LED on
-       delay(2000);
-       digitalWrite(4, LOW ); // Turn the LED off
-       delay(2000);
-    }
-    if (Rand==5){
-       digitalWrite(5, HIGH ); // Turn the LED on
-       delay(2000);
-       digitalWrite(5, LOW ); // Turn the LED off
-       delay(2000);
-    }
-    if (Rand==6){
-       digitalWrite(6, HIGH ); // Turn the LED on
-       delay(2000);
-       digitalWrite(6, LOW ); // Turn the LED off
-       delay(2000);
-    }
-    if (Rand==7){
-       digitalWrite(7, HIGH ); // Turn the LED on
-       delay(2000);
-       digitalWrite(7, LOW ); // Turn the LED off
-       delay(2000);
-    }
-    if (Rand==8){
-       digitalWrite(8, HIGH ); // Turn the LED on
-       delay(2000);
-       digitalWrite(8, LOW ); // Turn the LED off
-       delay(2000);
-    }
-    if (Rand==9){
-       digitalWrite(9, HIGH ); // Turn the LED on
-       delay(2000);
-       digitalWrite(9, LOW ); // Turn the LED off
-       delay(2000);
-    }
-    if (Rand==10){
-       digitalWrite(10, HIGH ); // Turn the LED on
-       delay(2000);
-       digitalWrite(10, LOW ); // Turn the LED off
-       delay(2000);
-    }   
+    while (digitalRead(buttonPin) == LOW) {
+      int Rand = random(2,10);   
+      if (Rand==2){
+         digitalWrite(2, HIGH ); // Turn the LED on
+         delay(2000);
+         digitalWrite(2, LOW ); // Turn the LED off
+         delay(2000);
+      }
+      if (Rand==3){
+         digitalWrite(3, HIGH ); // Turn the LED on
+         delay(2000);
+         digitalWrite(3, LOW ); // Turn the LED off
+         delay(2000);
+      }
+      if (Rand==4){
+         digitalWrite(4, HIGH ); // Turn the LED on
+         delay(2000);
+         digitalWrite(4, LOW ); // Turn the LED off
+         delay(2000);
+      }
+      if (Rand==5){
+         digitalWrite(5, HIGH ); // Turn the LED on
+         delay(2000);
+         digitalWrite(5, LOW ); // Turn the LED off
+         delay(2000);
+      }
+      if (Rand==6){
+         digitalWrite(6, HIGH ); // Turn the LED on
+         delay(2000);
+         digitalWrite(6, LOW ); // Turn the LED off
+         delay(2000);
+      }
+      if (Rand==7){
+         digitalWrite(7, HIGH ); // Turn the LED on
+         delay(2000);
+         digitalWrite(7, LOW ); // Turn the LED off
+         delay(2000);
+      }
+      if (Rand==8){
+         digitalWrite(8, HIGH ); // Turn the LED on
+         delay(2000);
+         digitalWrite(8, LOW ); // Turn the LED off
+         delay(2000);
+      }
+      if (Rand==9){
+         digitalWrite(9, HIGH ); // Turn the LED on
+         delay(2000);
+         digitalWrite(9, LOW ); // Turn the LED off
+         delay(2000);
+      }
+      if (Rand==10){
+         digitalWrite(10, HIGH ); // Turn the LED on
+         delay(2000);
+         digitalWrite(10, LOW ); // Turn the LED off
+         delay(2000);
+      }
+    }  
 }
